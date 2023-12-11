d40970b210b10a3a507dc958ae28f231d2614c24
pranav <pranavofficial404@gmail.com>
2023-12-11 12:51:19 +0530

    chore: ⚒️ some modificatios

--- example/server.ts
+++ example/server.ts
@@ -1,32 +1,22 @@
 import Response from "../src/response";
 import FastNet from "../src/fastnet";
 import Request from "../src/request";
+
 const app = new FastNet();
 
-interface CustomRequest extends Request {
-	hello: string;
-}
-
-app.use(async (req: CustomRequest, res: Response, next: any) => {
-	req.hello = "hello world";
-	// console.log('hello');
-	console.log(req.hello);
-
+app.use(async (req: Request, res: Response, next: any) => {
+	console.log("Hey I am called for no reason (middleware)");
 	await next();
 });
 
-app.get("/hello", async (req: CustomRequest, res: Response) => {
-	res.send(req.hello);
-}).post("/hello", (req: CustomRequest, res: Response) => {
-	res.send(req.hello);
+app.get("/hello", async (req: Request, res: Response) => {
+	res.send("hello world");
 });
 
-app.use(async (req: CustomRequest, res: Response, next: any) => {
-	req.hello = "world hello";
-	console.log(req.hello);
-	await next();
+app.post("/hello", async (req: Request, res: Response) => {
+	const body = await req.body;
+	res.send(body);
 });
-// app
 
 app.listen(3000, () => {
 	console.log("server is listening on port 3000");
